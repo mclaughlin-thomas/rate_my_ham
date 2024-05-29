5b0f5386eeac879661b0346aaba4fc1666901b7e
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2024-05-29 16:13:36 -0700

    Basic Routes

---
+++ lib/pages/terms_welcome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TermsWelcome extends StatelessWidget {
+  const TermsWelcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: const Text('Terms and Conditions'),),
+    );
+  }
+}
