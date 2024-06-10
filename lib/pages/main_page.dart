2e07037a21d9aca8a1ad2ed7e4b59c5206f3dfc6
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2024-06-09 21:32:12 -0400

    Method to wait until all firebase assets load added | working

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,171 +5,138 @@ import 'package:rate_my_ham/util/tinder_card.dart';
 
 
 class MainPage extends StatefulWidget {
-
   @override
   State<MainPage> createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
-  late String imageUrl1;
-  late String imageUrl2;
-  late String imageUrl3;
-  late String imageUrl4;
-  late String imageUrl5;
-  late String imageUrl6;
-  late String imageUrl7;
-  late String imageUrl8;
-  late String imageUrl9;
-  late String imageUrl10;
-  
+  bool isLoading = true;
+  List<String> imageUrls = [];
 
   final storage = FirebaseStorage.instance;
 
   @override
   void initState() {
     super.initState();
-    // set the initial value of the image url to empty string
-    imageUrl1 = '';
-    imageUrl2 = '';
-    imageUrl3 = '';
-    imageUrl4 = '';
-    imageUrl5 = '';
-    imageUrl6 = '';
-    imageUrl7 = '';
-    imageUrl8 = '';
-    imageUrl9 = '';
-    imageUrl10 = '';
-    //retrieve the image url from firebase storage
-    getImageUrl();
+    getImageUrls();
   }
 
-  Future<void> getImageUrl() async {
-    //get the reference to the mage file in Firebase storage
+  Future<void> getImageUrls() async {
     final ref1 = storage.ref().child('emiham.jpg');
     final ref2 = storage.ref().child('ham.jpg');
-    final ref3 = storage.ref().child('ham.jpg');
-    final ref4 = storage.ref().child('ham.jpg');
-    final ref5 = storage.ref().child('ham.jpg');
-    final ref6 = storage.ref().child('ham.jpg');
-    final ref7 = storage.ref().child('ham.jpg');
-    final ref8 = storage.ref().child('ham.jpg');
-    final ref9 = storage.ref().child('ham.jpg');
-    final ref10 = storage.ref().child('ham.jpg');
+    final ref3 = storage.ref().child('closeham.jpg');
+    final ref4 = storage.ref().child('kittenPlay.png');
+    final ref5 = storage.ref().child('strangeham.jpg'); // first image
+    // final ref6 = storage.ref().child('ham.jpg');
+    // final ref7 = storage.ref().child('ham.jpg');
+    // final ref8 = storage.ref().child('ham.jpg');
+    // final ref9 = storage.ref().child('ham.jpg');
+    // final ref10 = storage.ref().child('ham.jpg');
 
-    final url1 = await ref1.getDownloadURL();
-    final url2 = await ref2.getDownloadURL();
-    final url3 = await ref3.getDownloadURL();
-    final url4 = await ref4.getDownloadURL();
-    final url5 = await ref5.getDownloadURL();
-    final url6 = await ref6.getDownloadURL();
-    final url7 = await ref7.getDownloadURL();
-    final url8 = await ref8.getDownloadURL();
-    final url9 = await ref9.getDownloadURL();
-    final url10 = await ref10.getDownloadURL();
-    
-    //Get the imageUrl to download URL
+    final urls = await Future.wait([
+      ref1.getDownloadURL(),
+      ref2.getDownloadURL(),
+      ref3.getDownloadURL(),
+      ref4.getDownloadURL(),
+      ref5.getDownloadURL(),
+      // ref6.getDownloadURL(),
+      // ref7.getDownloadURL(),
+      // ref8.getDownloadURL(),
+      // ref9.getDownloadURL(),
+      // ref10.getDownloadURL(),
+    ]);
+
+    await Future.wait(urls.map((url) => precacheImage(NetworkImage(url), context)));
+
     setState(() {
-      imageUrl1 = url1;
-      imageUrl2 = url2;
-      imageUrl3 = url3;
-      imageUrl4 = url4;
-      imageUrl5 = url5;
-      imageUrl6 = url6;
-      imageUrl7 = url7;
-      imageUrl8 = url8;
-      imageUrl9 = url9;
-      imageUrl10 = url10;
+      imageUrls = urls;
+      isLoading = false;
     });
   }
 
+  @override
   Widget build(BuildContext context) {
     final String user_Name = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
       backgroundColor: Colors.pink[50],
       body: Center(
-        child: Stack(
-          children: [
-            CustomPaint(
-              painter: CirclePainter(),
-              child: Container(
-                color: Colors.transparent,
-                width: double.infinity,
-                height: double.infinity,
-              ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 5.0),
-                child: SingleChildScrollView( // Wrap your Column with SingleChildScrollView
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.data_exploration_rounded ),
-                            iconSize: 50.0,
-                            color: Colors.black,
-                            onPressed: () {},
-                          ),
-                          SizedBox(width: 1),
-                          IconButton(
-                            icon: Icon(Icons.info_rounded),
-                            iconSize: 50.0,
-                            color: Colors.black,
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                context,
-                                '/info'
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Image(image: NetworkImage(imageUrl1), height: 100, width: 100),
-                      Image(image: NetworkImage(imageUrl2), height: 100, width: 100),
-                      // Container(
-                      //   height: 500,
-                      //   width: 320,
-                      //   child: Stack(
-                      //     children: [
-                      //       TinderCard(imagePath: 'lib/images/emiham.jpg', userName: user_Name), //last picture
-                      //       TinderCard(imagePath: 'lib/images/strangeham.jpg' , userName: user_Name),
-                      //       TinderCard(imagePath: 'lib/images/ham.jpg' , userName: user_Name),
-                      //       TinderCard(imagePath: 'lib/images/closeham.jpg' , userName: user_Name), //first picture
-                      //     ],
-                      //   ),
-                      // ),
-                      SizedBox(height: 20), // Add some space between the stack and the buttons
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.thumb_down, color: Colors.red, size: 50.0),
-                            onPressed: () {
-                              // Handle thumbs down action
-                            },
-                          ),
-                          SizedBox(width: 50),
-                          IconButton(
-                            icon: Icon(Icons.thumb_up, color: Colors.green, size: 50.0),
-                            onPressed: () {
-                              // Handle thumbs up action
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Stack(
+                children: [
+                  CustomPaint(
+                    painter: CirclePainter(),
+                    child: Container(
+                      color: Colors.transparent,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
                   ),
-                ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 5.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.data_exploration_rounded),
+                                  iconSize: 50.0,
+                                  color: Colors.black,
+                                  onPressed: () {},
+                                ),
+                                SizedBox(width: 1),
+                                IconButton(
+                                  icon: Icon(Icons.info_rounded),
+                                  iconSize: 50.0,
+                                  color: Colors.black,
+                                  onPressed: () {
+                                    Navigator.pushNamed(context, '/info');
+                                  },
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 20),
+                            Container(
+                              height: 500,
+                              width: 320,
+                              child: Stack(
+                                children: [
+                                  for (var url in imageUrls)
+                                    TinderCard(imagePath: url, userName: user_Name),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.thumb_down, color: Colors.red, size: 50.0),
+                                  onPressed: () {
+                                    // Handle thumbs down action
+                                  },
+                                ),
+                                SizedBox(width: 50),
+                                IconButton(
+                                  icon: Icon(Icons.thumb_up, color: Colors.green, size: 50.0),
+                                  onPressed: () {
+                                    // Handle thumbs up action
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-        // add two buttons here 
-        
       ),
     );
   }
--- lib/util/tinder_card.dart
+++ lib/util/tinder_card.dart
@@ -5,13 +5,11 @@ import 'package:rate_my_ham/services/firestore.dart'; //FB
 
 
 class TinderCard extends StatelessWidget {
-  final String imagePath; // Replace with String for image path
-  final String userName; // Replace with String for name
-  final FireStoreService fireStoreService = FireStoreService(); //FB
-
+  final String imagePath;
+  final String userName;
+  final FireStoreService fireStoreService = FireStoreService();
 
   TinderCard({required this.imagePath, required this.userName});
-
 
   String getLastPartOfPath(String path) {
     return path.split('/').last;
@@ -21,20 +19,18 @@ class TinderCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Swipable(
       onSwipeRight: (finalPosition) {
-        print(' Swiped right ');
-        fireStoreService.createReview(getLastPartOfPath(imagePath), userName, "love"); //FB
-        print(' image was loved ');
-
+        print('Swiped right');
+        fireStoreService.createReview(getLastPartOfPath(imagePath), userName, "love");
+        print('Image was loved');
       },
       onSwipeLeft: (finalPosition) {
-        print(' Swiped left ');
-        fireStoreService.createReview(getLastPartOfPath(imagePath), userName, "like"); //FB
-        print(' image was liked ');
+        print('Swiped left');
+        fireStoreService.createReview(getLastPartOfPath(imagePath), userName, "like");
+        print('Image was liked');
       },
       verticalSwipe: false,
       child: Stack(
         children: [
-          // Black border with rounded corners
           ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: Container(
@@ -43,14 +39,30 @@ class TinderCard extends StatelessWidget {
               height: double.infinity,
             ),
           ),
-          // Image with slightly smaller rounded corners and inset padding
           ClipRRect(
-            borderRadius: BorderRadius.circular(40), // Slightly smaller radius
+            borderRadius: BorderRadius.circular(40),
             child: Padding(
-              padding: const EdgeInsets.all(2.0), // Add padding for border
-              child: Image.asset(
+              padding: const EdgeInsets.all(2.0),
+              child: Image.network(
                 imagePath,
                 fit: BoxFit.cover,
+                // I DID NOT WRITE THE BELOW CODE
+                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Center(
+                    child: CircularProgressIndicator(
+                      value: loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded / (loadingProgress.expectedTotalBytes ?? 1)
+                          : null,
+                    ),
+                  );
+                },
+                // I DID NOT WRITE THE ABOVE CODE
+                errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                  return Center(
+                    child: Icon(Icons.error)
+                  );
+                },
               ),
             ),
           ),
