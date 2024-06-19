50a190474ab4c9936993e2fe2d632a22629d4fb3
mclaughlin-thomas <thomas.mclaughlin2@stvincent.edu>
2024-06-19 18:44:15 -0400

    Removed routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rate_my_ham/pages/terms_welcome.dart';
-//import 'package:rate_my_ham/pages/main_page.dart';
-//import 'package:rate_my_ham/pages/info_page.dart';
-//import 'package:rate_my_ham/pages/select_user_name.dart';
-//import 'package:rate_my_ham/pages/analytics_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,19 +11,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: TermsWelcome(),
-      routes: {
-        //'/terms': (context) => TermsWelcome(),
-        //'/main': (context) => MainPage(),
-        //'/info': (context) => InfoPage(),
-        //'/select_user_name': (context) => SelectUserName(),
-        // '/analytics': (context) => AnalyticsPage(),
-      },
+
     );
   }
 }
